95432b704101ffd396d1bc001592339aeb803666
VCampos10 <VC195063@icloud.com>
2024-09-30 16:33:17 -0700

    ch 4 notes

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
-print ("Hello World")
-
-print ("Hello World!")
-
+import random
+i = 0; cnt = 0
+while ( i < 10):
+    rdnum = random.randint(0,100)
+    print(rdnum, end = ' ')
+    if (rdnum >= 0) and (rdnum <= 100):
+        cnt += 1
+    i += 1
+print(f'The total is {cnt:10d}')
